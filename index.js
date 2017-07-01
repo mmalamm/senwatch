3f80d34e914cca85f16d5a4606d9a08d3f3280e3
M M Alam <alam@mmtutor.com>
2017-07-01 17:45:43 -0400

    add api calls and remove sensitive info (refreshed accidentally committed api keys)

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var favicon = require('serve-favicon');
 var path = require('path');
-var secrets = require('./secrets');
-console.log(secrets);
+var sec = require('./secrets');
 var app = express();
 var port = process.env.PORT || 3000;
 
@@ -11,26 +10,30 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => console.log('Ayo big the server running on port ', port));
 
-var result;
+var crpCallResult;
 var request = require('request');
-request('http://www.opensecrets.org/api/?method=candContrib&output=json&cid=N00007360&cycle=2016&apikey=117704c060b0f63c93e04ad2aaaa0d08', function (error, response, body) {
+let crpKey = sec.secrets.crpKey || 'nunya';
+request(`http://www.opensecrets.org/api/?method=candContrib&output=json&cid=N00007360&cycle=2016&apikey=${crpKey}`, function (error, response, body) {
   // console.log('error:', error); // Print the error if one occurred
   // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  result = JSON.parse(body);
+  crpCallResult = JSON.parse(body);
 });
 
+var ppCallResult;
 var curl = require('curlrequest');
-
-let ppHeadersObj = secrets.secrets.ppHeader || 'nunya';
-console.log(ppHeadersObj);
+let ppHeadersObj = sec.secrets.ppHeader || 'nunya';
 curl.request(
   {
     url : 'https://api.propublica.org/congress/v1/115/senate/members.json',
     headers : ppHeadersObj
   }, (err, stdout) => {
-    console.log(stdout);
+    ppCallResult = JSON.parse(stdout);
   });
 
-app.get('/', (req, res) => {
-  res.json(result);
+app.get('/crp', (req, res) => {
+  res.json(crpCallResult);
+});
+
+app.get('/pp', (req, res) => {
+  res.json(ppCallResult);
 });
