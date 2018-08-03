75154d096f1d630355071a15f7de23faeddd6740
M M Alam <alam@mmtutor.com>
2018-08-03 17:36:59 -0400

    refactor init commit

---
+++ senwatch_react/src/App/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <svg className="map-svg" width="960" height="600" />
+        <div>Hello Senwatch</div>
+      </div>
+    );
+  }
+}
+
+export default App;
