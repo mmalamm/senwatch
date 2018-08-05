f8fffdc2b20de38510bc481d9a0fd00983af0600
Muhammad Alam <alam@mmtutor.com>
2018-08-05 01:58:10 -0400

    refactor changes

---
+++ senwatch_react/src/Components/App/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.css";
+
+import Map from "../Map/Map";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Map />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ senwatch_react/src/Components/Map/Map.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import "./Map.css";
+
+import { geoPath, json } from "d3";
+import { feature } from "topojson-client";
+import stateHash from "./statesInfo.js";
+const { stateId2CodeMap } = stateHash;
+
+class StateSVG extends Component {
+  constructor(props) {
+    super(props);
+    const i = props.i;
+    this.stateCode = stateId2CodeMap[i];
+  }
+  render() {
+    const { i, d } = this.props;
+    return <path d={d} uid={i} />;
+  }
+}
+
+class Map extends Component {
+  state = {
+    us: { objects: { states: [] } },
+    loaded: false
+  };
+  componentDidMount = async function() {
+    const usJson = await json("https://d3js.org/us-10m.v1.json");
+    this.setState({ us: usJson, loaded: true });
+  };
+  render() {
+    const path = geoPath();
+    const { loaded, us } = this.state;
+    const { features } = feature(us, us.objects.states);
+    return (
+      <svg className="map-svg">
+        <g className="states">
+          {loaded &&
+            features.map(path).map((p, i) => <StateSVG key={i} i={i} d={p} />)}
+        </g>
+      </svg>
+    );
+  }
+}
+
+export default Map;
