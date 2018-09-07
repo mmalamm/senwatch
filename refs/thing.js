dbb84c94532f931d66ebd2b4817b26b6e2a26cd2
M M Alam <alam@mmtutor.com>
2018-09-07 15:08:18 -0400

    add old sen schema to refs

---
+++ refs/thing.js
@@ -0,0 +1,168 @@
+const sen = {
+  _id: "59705689392334118a72c334",
+  pp_id: "B001288",
+  first_name: "Cory",
+  middle_name: null,
+  last_name: "Booker",
+  party: "D",
+  twitter_account: "CoryBooker",
+  facebook_account: "",
+  rss_url: "https://www.booker.senate.gov/rss/",
+  crp_id: "N00035267",
+  crp: {
+    candIndustry: {
+      "@attributes": {
+        cand_name: "Cory Booker (D)",
+        cid: "N00035267",
+        cycle: "2016",
+        origin: "Center for Responsive Politics",
+        source:
+          "http://www.opensecrets.org/politicians/industries.php?cid=N00035267&cycle=2010",
+        last_updated: "05/16/2017"
+      },
+      industry: [
+        {
+          "@attributes": {
+            industry_code: "K01",
+            industry_name: "Lawyers/Law Firms",
+            indivs: "2314675",
+            pacs: "184500",
+            total: "2499175"
+          }
+        },
+        {
+          "@attributes": {
+            industry_code: "F07",
+            industry_name: "Securities & Investment",
+            indivs: "2269477",
+            pacs: "61200",
+            total: "2330677"
+          }
+        },
+        {
+          "@attributes": {
+            industry_code: "F10",
+            industry_name: "Real Estate",
+            indivs: "1172760",
+            pacs: "29000",
+            total: "1201760"
+          }
+        },
+        {
+          "@attributes": {
+            industry_code: "B02",
+            industry_name: "TV/Movies/Music",
+            indivs: "728162",
+            pacs: "32000",
+            total: "760162"
+          }
+        },
+        {
+          "@attributes": {
+            industry_code: "F13",
+            industry_name: "Misc Finance",
+            indivs: "505825",
+            pacs: "15000",
+            total: "520825"
+          }
+        },
+        {
+          "@attributes": {
+            industry_code: "N05",
+            industry_name: "Business Services",
+            indivs: "478856",
+            pacs: "10500",
+            total: "489356"
+          }
+        },
+        {
+          "@attributes": {
+            industry_code: "Q05",
+            industry_name: "Pro-Israel",
+            indivs: "397599",
+            pacs: "36527",
+            total: "434126"
+          }
+        },
+        {
+          "@attributes": {
+            industry_code: "H04",
+            industry_name: "Pharmaceuticals/Health Products",
+            indivs: "173841",
+            pacs: "218500",
+            total: "392341"
+          }
+        },
+        {
+          "@attributes": {
+            industry_code: "W06",
+            industry_name: "Retired",
+            indivs: "376988",
+            pacs: "0",
+            total: "376988"
+          }
+        },
+        {
+          "@attributes": {
+            industry_code: "F09",
+            industry_name: "Insurance",
+            indivs: "249035",
+            pacs: "124000",
+            total: "373035"
+          }
+        }
+      ]
+    }
+  },
+  domain: "www.booker.senate.gov/",
+  next_election: "2020",
+  phone: "202-224-3224",
+  fax: "202-224-8378",
+  state: "NJ",
+  state_rank: "junior",
+  senate_class: "2",
+  dob: "1969-04-27",
+  gender: "M",
+  committees: [
+    {
+      name: "Committee on Commerce, Science, and Transportation",
+      code: "SSCM",
+      api_uri:
+        "https://api.propublica.org/congress/v1/115/senate/committees/SSCM.json",
+      rank_in_party: null,
+      begin_date: "2017-01-03",
+      end_date: "2019-01-03"
+    },
+    {
+      name: "Committee on Environment and Public Works",
+      code: "SSEV",
+      api_uri:
+        "https://api.propublica.org/congress/v1/115/senate/committees/SSEV.json",
+      rank_in_party: null,
+      begin_date: "2017-01-03",
+      end_date: "2019-01-03"
+    },
+    {
+      name: "Committee on Foreign Relations",
+      code: "SSFR",
+      api_uri:
+        "https://api.propublica.org/congress/v1/115/senate/committees/SSFR.json",
+      rank_in_party: null,
+      begin_date: "2017-01-03",
+      end_date: "2019-01-03"
+    },
+    {
+      name: "Committee on Small Business and Entrepreneurship",
+      code: "SSSB",
+      api_uri:
+        "https://api.propublica.org/congress/v1/115/senate/committees/SSSB.json",
+      rank_in_party: null,
+      begin_date: "2017-01-03",
+      end_date: "2019-01-03"
+    }
+  ],
+  img_url:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Cory_Booker%2C_official_portrait%2C_114th_Congress.jpg/500px-Cory_Booker%2C_official_portrait%2C_114th_Congress.jpg",
+  office: "359 Dirksen Senate Office Building",
+  votes_with_party_pct: 83.71
+};
